119805274969ff8aaa75c8a88a6bce511480ddb7
FrugenceFide <frugencefidel@gmail.com>
2020-03-27 18:38:38 +0300

    Add colours

--- lib/theme.ts
+++ lib/theme.ts
@@ -107,8 +107,17 @@ const theme: DefaultTheme = {
     s256: '25.6rem'
   },
   colors: {
-    blue: {
-      primary: '#008DC9'
+    green: {
+      primary: '#4caf50'
+    },
+    black: {
+      primary: '#212b36'
+    },
+    orange: {
+      primary: '#f77427'
+    },
+    red: {
+      primary: '#ec314b'
     }
   }
 };
