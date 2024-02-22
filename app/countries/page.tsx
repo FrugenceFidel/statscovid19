9f6011e9f1640fcb5f9d43bcf8ba4429b76e16f1
Frugence Fidel <frugencefidel@gmail.com>
2024-02-22 05:04:35 +0300

    countries page

---
+++ app/countries/page.tsx
@@ -0,0 +1,5 @@
+import SearchCountry from '@/components/SearchCountry';
+
+const CountriesPage: React.FC = () => <SearchCountry />;
+
+export default CountriesPage;
--- pages/countries.tsx
+++
@@ -1,6 +0,0 @@
-import { NextPage } from 'next';
-import SearchCountry from '../components/SearchCountry';
-
-const Home: NextPage = () => <SearchCountry />;
-
-export default Home;
