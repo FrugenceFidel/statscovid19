841ec842bc1a0a0b2d5a75eec9abadf270f01085
Frugence Fidel <frugencefidel@gmail.com>
2024-02-22 03:25:08 +0300

    Add app router

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+export const metadata = {
+  title: 'COVID-19 STATS',
+  description:
+    'Covid-19 | Coronavirus disease 2019 (COVID-19) is an infectious disease caused by severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2)',
+  keywords: ['Next.js', 'React'],
+  authors: [{ name: 'Frugence Fidel', url: 'https://frugencefidel.com' }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+import Hero from '@/components/Hero';
+import MyCountryCases from '@/components/MyCountryCases';
+
+const Home: FC = () => {
+  return (
+    <>
+      <p>Home</p>
+      {/* <Hero />
+      <MyCountryCases /> */}
+    </>
+  );
+};
+
+export default Home;
